1a74d80d3f054a051bb3417663a2e6cd2bb9624a
shradsgithub <shradsjobs@gmail.com>
2023-04-25 17:08:01 +0530

    Jcommit

---
+++ JenkinsTest/src/test/java/jenkinsTesting/JenkinsBuildTest.java
@@ -0,0 +1,13 @@
+package jenkinsTesting;
+
+import org.testng.annotations.Test;
+
+public class JenkinsBuildTest {
+	
+	@Test
+	public void method()
+	{
+		System.out.println("Through Jenkins we are trying to execute ");
+	}
+
+}
